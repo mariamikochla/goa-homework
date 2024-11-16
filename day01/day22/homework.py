cf68c2950cfb4de726de44aa9c5029018037dc18
mariamikochla <kochlamazashvilimariam95@gmail.com>
2024-11-16 19:36:58 +0400

    commit

---
+++ day01/day22/homework.py
@@ -0,0 +1,57 @@
+
+
+for num in range(101):
+    print(num)
+
+
+
+
+for num in range(20, 41):
+    print(num)
+
+
+
+for num in range(75, 101, 2):
+    print(num)
+
+
+
+last_name="kochlamazashvili"
+for letter in last_name:
+    print(letter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day06/homework/lesson6.py
@@ -0,0 +1,11 @@
+user_name = input("whatis your name: ")
+
+last_name = input("what is your last name")
+
+user_home = input("where do you lave: ")
+
+date_of_birth = input("what year were you orn")
+
+user_high = input("how tall are you: ")
+
+print( user_name +" "+last_name+" "+ user_home+" "+ date_of_birth+" "+ user_high )
